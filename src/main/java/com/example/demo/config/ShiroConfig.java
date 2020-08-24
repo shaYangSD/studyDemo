4b8c3fc7b28a22ac9762c6a2dd8cf3e40381d89e
rongwenjun <rongwj@Galanz.com>
2020-08-24 18:04:54 +0800

    spring项目第二次提交

--- src/main/java/com/example/demo/config/ShiroConfig.java
+++ src/main/java/com/example/demo/config/ShiroConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.filter.JWTFilter;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.springframework.context.annotation.Bean;
 
@@ -13,15 +12,12 @@ public class ShiroConfig {
      * 先经过token过滤器，如果检测到请求头存在 token，则用 token 去 login，接着走 Realm 去验证
      * */
     @Bean
-    public ShiroFilterFactoryBean factory(SecurityManager securityManager){
+    public void factory(SecurityManager securityManager){
         ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
 
         //添加自己的过滤器取名为jwt
         Map<String, Filter> filterMap=new LinkedHashMap<>();//??????
         //设置自定义的jwt过滤器
-        filterMap.put("jwt",new JWTFilter() );
-//http://www.cocoachina.com/articles/32386/uu
-
     }
 
 }
